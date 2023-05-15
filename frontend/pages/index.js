e6ddb8b3d83f17bc920e372bc6843398cbc1a2e9
nikola-s6 <nikola.stojilkovic6@gmail.com>
2023-05-15 22:33:39 +0200

    getCredentials contract function call

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,29 @@
 import HomeLoggedIn from "@/components/HomeLoggedIn"
 import HomeLoggedOut from "@/components/HomeLoggedOut"
-import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { abi, address } from "@/constants"
 
 export default function Home() {
-  const { isWeb3Enabled } = useMoralis()
-  // return <>{isWeb3Enabled ? <HomeLoggedIn /> : <HomeLoggedOut />}</>
-  return <>{window.localStorage.getItem("connected") ? <HomeLoggedIn /> : <HomeLoggedOut />}</>
+  const { chainId: chainIdHex, account } = useMoralis() // chain id is in hex
+  const [credentials, setCredentials] = useState("")
+  const chainId = parseInt(chainIdHex)
+  const contractAddress = chainId in address ? address[chainId] : null
+
+  const { runContractFunction: getCredentials } = useWeb3Contract({
+    abi: abi,
+    contractAddress: contractAddress,
+    functionName: "getCredentials",
+    params: {},
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      let cred = await getCredentials()
+      setCredentials(cred)
+    }
+    fetchData()
+  }, [account])
+
+  return <>{account ? <HomeLoggedIn /> : <HomeLoggedOut />}</>
 }
