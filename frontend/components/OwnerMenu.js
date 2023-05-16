20a2513a63ec67fd1293e95dd553e691cc76f9b8
nikola-s6 <nikola.stojilkovic6@gmail.com>
2023-05-16 22:53:23 +0200

    updated admin creation form

--- frontend/components/OwnerMenu.js
+++ frontend/components/OwnerMenu.js
@@ -6,7 +6,7 @@ export default function OwnerMenu() {
       <Link href="/admin/create" className="mr-auto ml-auto">
         <button
           type="button"
-          class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           Create a admin
         </button>
@@ -14,7 +14,7 @@ export default function OwnerMenu() {
       <Link href="/doctor/create" className="mr-auto ml-auto">
         <button
           type="button"
-          class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           Create a doctor
         </button>
@@ -22,7 +22,7 @@ export default function OwnerMenu() {
       <Link href="/patient/create" className="mr-auto ml-auto">
         <button
           type="button"
-          class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           Create a patient
         </button>
--- frontend/pages/admin/create.js
+++ frontend/pages/admin/create.js
@@ -20,106 +20,176 @@ export default function AdminCreate() {
 
   const { Moralis } = useMoralis()
 
+  const handleSubmit = async function (e) {
+    e.preventDefault()
+
+    const sendOptions = {
+      abi: abi,
+      contractAddress: contractAddress,
+      functionName: "addAdmin",
+      params: {
+        _adminAddress: walletAddress,
+        _firstName: firstName,
+        _lastName: lastName,
+        _phoneNumber: email,
+        _email: phoneNumber,
+        _rAddress: residentialAddress,
+      },
+    }
+    console.log(sendOptions)
+
+    const transaction = await Moralis.executeFunction(sendOptions)
+    const txReceipt = await transaction.wait()
+    console.log(transaction)
+    console.log(txReceipt)
+
+    dispatch({
+      type: "info",
+      message: "Admin is successfully added",
+      title: "Transaction complete",
+      position: "bottomR",
+      icon: "bell",
+    })
+
+    if (document) {
+      const form = document.getElementById("form")
+      form.reset()
+    }
+  }
+
   return (
     <>
       <OwnerMenu></OwnerMenu>
-      <div className="mx-[20vw] my-10">
-        <Form
-          buttonConfig={{
-            onClick: function noRefCheck() {},
-            theme: "primary",
-          }}
-          data={[
-            {
-              inputWidth: "100%",
-              name: "wallet address",
-              type: "text",
-              value: "",
-              id: "adresa",
-              validation: {
-                required: true,
-              },
-            },
-            {
-              inputWidth: "100%",
-              name: "first name",
-              type: "text",
-              value: "",
-              validation: {
-                required: true,
-              },
-            },
-            {
-              inputWidth: "100%",
-              name: "last name",
-              type: "text",
-              value: "",
-              validation: {
-                required: true,
-              },
-            },
-            {
-              inputWidth: "100%",
-              name: "phone number",
-              type: "text",
-              value: "+381",
-              validation: {
-                // regExp: "^381^[0-9]+$",
-                required: true,
-              },
-            },
-            {
-              inputWidth: "100%",
-              name: "your email",
-              type: "email",
-              validation: {
-                // regExp: "^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$",
-                required: true,
-              },
-              value: "",
-            },
-            {
-              inputWidth: "100%",
-              name: "residential address",
-              type: "text",
-              value: "",
-              validation: {
-                required: true,
-              },
-            },
-          ]}
-          onSubmit={async function noRefCheck(data) {
-            console.log(data)
-
-            const sendOptions = {
-              abi: abi,
-              contractAddress: contractAddress,
-              functionName: "addAdmin",
-              params: {
-                _adminAddress: data[0].inputResult,
-                _firstName: data[1].inputResult,
-                _lastName: data[2].inputResult,
-                _phoneNumber: data[3].inputResult,
-                _email: data[4].inputResult,
-                _rAddress: data[5].inputResult,
-              },
-            }
-
-            const transaction = await Moralis.executeFunction(sendOptions)
-            await transaction.wait()
-            console.log(transaction)
-
-            dispatch({
-              type: "info",
-              message: "Admin is successfully added",
-              title: "Transaction complete",
-              position: "bottomR",
-              icon: "bell",
-            })
-          }}
-          title="Create admin:"
-          id="forma"
-        />
+      <div className="bg-white rounded-lg p-5 my-10 w-[80vw] mx-auto">
+        <h1 className="mb-5 border-double border-b-4 border-black text-xl mb-8">Create a admin:</h1>
+        <form className="text-black" onSubmit={handleSubmit} id="form">
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              type="text"
+              name="floating_address"
+              id="floating_address"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              onChange={(e) => {
+                setWalletAddress(e.target.value)
+              }}
+              required
+            />
+            <label
+              htmlFor="floating_address"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Wallet address
+            </label>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="text"
+                name="floating_first_name"
+                id="floating_first_name"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                onChange={(e) => {
+                  setFirstName(e.target.value)
+                }}
+                required
+              />
+              <label
+                htmlFor="floating_first_name"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                First name
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="text"
+                name="floating_last_name"
+                id="floating_last_name"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                onChange={(e) => {
+                  setLastName(e.target.value)
+                }}
+                required
+              />
+              <label
+                htmlFor="floating_last_name"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Last name
+              </label>
+            </div>
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              type="email"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+              required
+            />
+            <label
+              htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email
+            </label>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="tel"
+                pattern="^\+(381){1}[0-9]{3,14}$"
+                name="floating_phone"
+                id="floating_phone"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                defaultValue="+381"
+                onChange={(e) => {
+                  setPhoneNumber(e.target.value)
+                }}
+                required
+              />
+              <label
+                htmlFor="floating_phone"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Phone number
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="text"
+                name="floating_residential_address"
+                id="floating_residential_address"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                onChange={(e) => {
+                  setResidentialAddress(e.target.value)
+                }}
+                required
+              />
+              <label
+                htmlFor="floating_residential_address"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Residential address
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </>
   )
